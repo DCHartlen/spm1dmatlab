801a569ced8579627cf65a54d78349c6b88ac977
Devon <Devon.Hartlen@uwaterloo.ca>
2023-04-19 13:15:26 -0400

    Updates to two sample permuter to ensure determinism
    Existing method had issues of ill-defined distributions and inconsistent thresholds run after run for smallish numbers of permutations (<1000). Using the existing randperm() approach, upwards of 40% of label permutations would be non-unique. 
    
    Revised approach brute forces a unique list of permutations when the total number of permutations is less than 10k to ensure consistent run-to-run performance and a fully defined distribution. There is some memory overhead with this approach, but it is not inordinate for less than 10k unique label permutations.

--- +spm1d/+stats/+nonparam/+permuters/APermuterTwoSample.m
+++ +spm1d/+stats/+nonparam/+permuters/APermuterTwoSample.m
@@ -40,13 +40,51 @@ classdef APermuterTwoSample < spm1d.stats.nonparam.permuters.APermuter
                     Z(i,:) = self.get_test_stat_ones( ONES(i,:)' );
                 end
             else
-                n        = iterations;
-                Z        = zeros(n, self.Q);
-                for i = 1:n
-                    ONES = randperm(self.J, self.JA);
-                    Z(i,:) = self.get_test_stat_ones( ONES' );
+                % % Original method of building PDF
+                % n        = iterations;
+                % Z        = zeros(n, self.Q);
+                % for i = 1:n
+                %     ONES = randperm(self.J, self.JA);
+                %     Z(i,:) = self.get_test_stat_ones( ONES' );
+                % end
+
+                % DCHartlen Modification (04-2023):
+                % Use of randperm presented issues at low sample counts, as
+                % there is a high risk of repeated label permutations. This
+                % is because method permuted indices, not labels, with
+                % repeatition. Test with small numbers of permutations
+                % (<1000) found upwards of 40% repeated labels, leading to
+                % issues with undefined distributions and very evident
+                % non-determinism between runs.
+                %
+                % Revised methodology creates a list of non-repeating
+                % label permutations, if and only if, the maximum number of
+                % label permutations is less than 10K. If more than 10K,
+                % fall bacak on random method. This method uses more
+                % memory, but a 10k long array is generally managable
+
+                n = iterations;
+                Z = zeros(n, self.Q);
+                % Use random method when number of permutions is v. high.
+                if n > 10000
+                    % Random perm method
+                    for i = 1:n
+                        ONES = randperm(self.J, self.JA);
+                        Z(i,:) = self.get_test_stat_ones( ONES' );
+                    end
+                % If number of permutes is low, create a list and cycle
+                % through
+                else
+                    % Generate random combination of "ones" 
+                    permutes = nchoosek(1:self.J,self.JA); 
+                    % Cycle through each permutation
+                    for iPerm = 1:n
+                        Z(iPerm,:) = ...
+                            self.get_test_stat_ones(permutes(iPerm,:)');
+                    end
                 end
             end
+            
             self.Z         = max(Z, [], 2);
             if self.dim == 1
                 self.ZZ    = Z;
